8bf8e8f81a98db6cb0ea8b7e517c0247196d452c
FazinHan <freak.fizaan@gmail.com>
2020-04-17 20:41:43 +0530

    Finished guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -1,18 +1,18 @@
 import random
-n = 20
-to_be_guessed = int(n * random.random()) + 1
+to_be_guessed = random.randint(0, 10)
 guess = 0
 while guess != to_be_guessed:
-    guess = input("New number: ")
+    guess = input("New number('q' to give up): ")
     if guess > 0:
         if guess > to_be_guessed:
             print "Number too large"
-        else:
+        elif guess == to_be_guessed:
+			print "Congratulations, you did it!"
+		else:
             print "Number too small"
-    else:
+    elif guess == 'q':
         print "Sorry that you're giving up!"
-        break
-	else:
-		print "Congratulations, you did it!"
+    else:
+		print "Invalid input"
 ###
 input('Press enter to quit')
