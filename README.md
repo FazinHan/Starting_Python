983ce82dbda7224cafa8065645fb229c318abbe4
FazinHan <freak.fizaan@gmail.com>
2020-05-26 15:37:52 +0530

    Improved quadratic.py and added graphing function. Readme updated accordingly.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Starting_python
+# Starting python
 This is a repo containing all my beginner python code. I wrote these while learning (although, admittedly, 
 I have had some prior experience with python). 
 I used Python version 3.8 for all the code in this repository.
@@ -9,3 +9,8 @@ The modules imported in code so far are:
 1. Math
 2. Numpy
 3. Random
+4. Matplotlib
+
+---
+
+For help with installing python, check out the [documentation](https://docs.python.org/3/installing/index.html) here.
--- quadratic.py
+++ quadratic.py
@@ -1,10 +1,21 @@
-import math as mt
+import matplotlib.pyplot as plt
+import numpy as np
+
 print('Quadratic solver')
 print('Of the form ax^2+bx+c=0')
 a = int(input('Please input a>>>'))
 b = int(input('Please input b>>>'))
 c = int(input('Please input c>>>'))
 D = b**2 - (4*a*c)
+min_at = -b/(2*a)
+x = np.linspace(min_at-a,min_at+a,700)
+y1 = a*(x**2)+b*x+c
+if a >= 0:
+    X = np.linspace(-y1[0],y1[0],2)
+else:
+    X = np.linspace(y1[0],-y1[0],2)
+y = np.linspace(0,0,2)
+
 if D == 0:
 	x = (-b)/(2*a)
 	print('The roots are real, repeating and are equal to')
@@ -12,8 +23,11 @@ if D == 0:
 if D < 0:
 	print('The roots are imaginary')
 if D > 0:
-	x1 = (-b + mt.sqrt(D))/(2*a)
-	x2 = (-b - mt.sqrt(D))/(2*a)
+	x1 = (-b + (D**0.5))/(2*a)
+	x2 = (-b - (D**0.5))/(2*a)
 	print('The roots are real, and are')
 	print(x1, x2)
-x = input('Press enter to continue')
+plt.plot(X,y,color='black')
+plt.plot(y,X,color='black')
+plt.plot(x,y1,color='magenta')
+plt.show()
